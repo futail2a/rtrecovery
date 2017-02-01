aae54e6c1fa137a3e3e025256f39213a7b5066b0
ogata <futail2a@gmail.com>
2017-02-01 12:33:29 +0900

    add comments in repgroup

--- repgroup.py
+++ repgroup.py
@@ -3,9 +3,15 @@ from rtshell import rtconf
 
 
 class ReplicaGroup:
-    #The group of components which share same config parameter "group_name"
-    #This class manages members of groups and thier primary components state 
-    
+    """
+    The group of components which share same config parameter "group_name"
+    This class manages members of groups and thier primary components state 
+    @param name: group name
+    @param comp_dic: value:component key:priority
+    @param primary_conns: Array of Connection
+    @param current_primary: Current primary component
+    @param primary_state: Internal states of the primary component
+    """
     def __init__(self, name):
         self.name              = name
         self.comp_dic          = {}
@@ -16,23 +22,28 @@ class ReplicaGroup:
         self.primary_state = {}
     
     def find_primary_comp(self):
-        #Set the smallest priority component as a current primary comp
+        """
+        Set the smallest priority component as a current primary comp
+        """
         self.current_primary = self.comp_dic[min(self.comp_dic.keys())]
         
     def load_primary_state(self):
-        
         for conf in self.current_primary.configuration_sets[0].configuration_data:
             self.primary_state[conf.name] = conf.data
     
     def find_next_primary(self):
-        #Exclude the old primary comp and set new primary comp
+        """
+        Exclude the old primary component and set new primary component
+        """
         self.comp_dic.pop(min(self.comp_dic.keys()))
         self.current_primary = self.comp_dic[min(self.comp_dic.keys())]            
         return self.current_primary               
         
     def switch_next_primary(self):
-        #Assumption that primary comp and replicate comps have same ports
-        #Switch connection source from old primary to new primary 
+        """
+        Assumption that primary comp and replicate comps have same ports
+        Switch connection source from old primary to new primary 
+        """
         try:
             for conn in self.primary_conns:
                 conn.change_source_comp(self.current_primary)
@@ -43,7 +54,9 @@ class ReplicaGroup:
         return 1
         
     def set_state_for_reps(self,t):
-        #Broadcast the states of primary to replicates
+        """
+        Broadcast the states of primary to replicates
+        """
         for k in self.comp_dic.keys():
             if not k == min(self.comp_dic.keys()):
                 for param, value in self.primary_state.iteritems():
